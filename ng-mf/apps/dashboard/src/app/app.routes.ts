b7d15204a60fefd407925b816a742367856456b6
slava-danilchak <danilchakv@gmail.com>
2025-05-06 20:24:44 +0300

    Updates

--- ng-mf/apps/dashboard/src/app/app.routes.ts
+++ ng-mf/apps/dashboard/src/app/app.routes.ts
@@ -1,13 +1,15 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () =>
+      loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
-    component: NxWelcomeComponent,
+    component: AppComponent,
   },
 ];
--- ng-mf/apps/dashboard/src/main.ts
+++ ng-mf/apps/dashboard/src/main.ts
@@ -1,1 +1,16 @@
-import('./bootstrap').catch((err) => console.error(err));
+// import('./bootstrap').catch((err) => console.error(err));
+
+
+// import { registerRemotes } from '@module-federation/enhanced/runtime';
+
+// fetch('/module-federation.manifest.json')
+//   .then((res) => res.json())
+//   .then((definitions) => registerRemotes(definitions))
+//   .then(() => import('./bootstrap').catch((err) => console.error(err)));
+  
+import { setRemoteDefinitions } from '@nx/angular/mf';
+
+fetch('/module-federation.manifest.json')
+  .then((res) => res.json())
+  .then((definitions) => setRemoteDefinitions(definitions))
+  .then(() => import('./bootstrap').catch((err) => console.error(err)));
