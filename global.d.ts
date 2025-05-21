503fc01a33115846ab7961dd76b623ce744270ed
slava-danilchak <danilchakv@gmail.com>
2025-05-21 18:53:18 +0300

    Remove full application

--- global.d.ts
+++
@@ -1,2 +0,0 @@
-declare module '@todoist/data';
-declare module '@todoist/ui'; 
--- libs/todoist-app/ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/button/button.component';
-export * from './lib/task-card/task-card.component';
-export * from './lib/modal/modal.component';
