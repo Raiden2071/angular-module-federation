0e612f8974461da717c55386aa34707d004aafef
slava-danilchak <danilchakv@gmail.com>
2025-05-30 11:44:53 +0300

    Moved code from layout to the inactivity service

--- angular/src/app/components/layout/layout.component.ts
+++ angular/src/app/components/layout/layout.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, computed, DestroyRef, HostBinding, HostListener, inject, NgZone, OnDestroy, OnInit, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, HostBinding, HostListener, inject, OnInit } from '@angular/core';
+import { InactivityService } from '../../services/inactivity/inactivity.service';
 import { RouterOutlet } from '@angular/router';
-import { debounceTime, fromEvent, merge } from 'rxjs';
-import { AuthService } from '../../services/auth/auth.service';
 
 export const OneMinute = 60 * 1000;
 export const TimeWhenWillBeLoggedOut = 0.05 * OneMinute;
@@ -12,67 +10,20 @@ export const TimeWhenWillBeLoggedOut = 0.05 * OneMinute;
   imports: [RouterOutlet],
   template: `<router-outlet />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [InactivityService]
 })
-export class LayoutComponent implements OnInit, OnDestroy {
-  private readonly destroyRef = inject(DestroyRef);
-  private readonly authService = inject(AuthService);
-  private readonly ngZone = inject(NgZone);
+export class LayoutComponent implements OnInit {
+  private readonly inactivityService = inject(InactivityService);
 
-  private interval: ReturnType<typeof setInterval> | null = null;
-  private lastUserActionTime = signal(new Date());
-  private logoutTime = computed(() => new Date(this.lastUserActionTime().getTime() + TimeWhenWillBeLoggedOut));
 
   @HostBinding('style.backgroundColor') color = 'red';
 
-  @HostListener('click', ['$event'])
+  @HostListener('dblclick', ['$event'])
   onMouseClick(event: MouseEvent) {
-    console.log(event);
-  }
-
-  constructor() {
+    console.log('double click');
   }
 
   ngOnInit(): void {
-    this.initListenerInactivity();
-  }
-
-  private initListenerInactivity(): void {
-    this.ngZone.runOutsideAngular(() => {
-
-      this.interval = setInterval(() => {
-        console.log(`
-          lastUserActionTime: ${this.lastUserActionTime().toISOString()}
-          logoutTime: ${this.logoutTime().toISOString()}
-          isLoggedOut: ${new Date() > this.logoutTime()}
-        `);
-
-        if (new Date() > this.logoutTime() ) {
-          this.authService.logout();
-        }
-      }, 1500);
-  
-      merge(
-        fromEvent(document, 'mousemove'),
-        fromEvent(document, 'click'),
-        fromEvent(document, 'scroll')
-      ).pipe(
-        debounceTime(500),
-        takeUntilDestroyed(this.destroyRef)
-      ).subscribe((event) => {
-        console.log(event);
-        this.updateLastUserActivity()
-      });
-    });
-  }
-
-  private updateLastUserActivity(): void {
-    this.lastUserActionTime.set(new Date());
-    console.log('logoutTime', this.logoutTime());
-  }
-
-  ngOnDestroy(): void {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
+    this.inactivityService.initListenerInactivity();
   }
 }
--- angular/src/app/services/inactivity/inactivity.service.ts
+++ angular/src/app/services/inactivity/inactivity.service.ts
@@ -1,10 +1,56 @@
-import { Injectable } from '@angular/core';
+import { computed, inject, NgZone, Injectable, signal, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AuthService } from '../auth/auth.service';
+import { TimeWhenWillBeLoggedOut } from '../../components/layout/layout.component';
+import { debounceTime } from 'rxjs/operators';
+import { concat, fromEvent } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class InactivityService {
+  private readonly authService = inject(AuthService);
+  private readonly ngZone = inject(NgZone);
+  private readonly destroyRef = inject(DestroyRef);
 
-  constructor() { }
+  private interval: ReturnType<typeof setInterval> | null = null;
+  private lastUserActionTime = signal(new Date());
+  private logoutTime = computed(() => new Date(this.lastUserActionTime().getTime() + TimeWhenWillBeLoggedOut));
 
+  constructor() {
+    this.destroyRef.onDestroy(() => {
+      this.clearInterval();
+    });
+  }
+
+  public initListenerInactivity(): void {
+    this.ngZone.runOutsideAngular(() => {
+
+      this.interval = setInterval(() => {
+
+        if (new Date() > this.logoutTime() ) {
+          this.authService.logout();
+        }
+      }, 1500);
+  
+      concat(
+        fromEvent(document, 'mousemove'),
+        fromEvent(document, 'click'),
+        fromEvent(document, 'scroll')
+      ).pipe(
+        debounceTime(200),
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe(() => {
+        this.updateLastUserActivity()
+      });
+    });
+  }
+  
+  private clearInterval(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  private updateLastUserActivity(): void {
+    this.lastUserActionTime.set(new Date());
+  }
 }
