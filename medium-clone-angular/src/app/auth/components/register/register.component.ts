3dd0619b3772504a19618a9de3c9f1b8ea988c31
slava-danilchak <danilchakv@gmail.com>
2025-06-04 17:06:34 +0300

    Have added layour for register component

--- medium-clone-angular/src/app/auth/components/register/register.component.ts
+++ medium-clone-angular/src/app/auth/components/register/register.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
-  //   styleUrls: ['./register.component.scss'],
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+  }
+}
