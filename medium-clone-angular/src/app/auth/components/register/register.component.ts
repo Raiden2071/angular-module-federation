f591c929ced48ce9ac7f5b70b890577f7bf354c6
slava-danilchak <danilchakv@gmail.com>
2025-06-07 11:41:07 +0300

    Disable button if isSubmitting equal false. Update angular.json

--- medium-clone-angular/src/app/auth/components/register/register.component.ts
+++ medium-clone-angular/src/app/auth/components/register/register.component.ts
@@ -1,17 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { selectIsSubmitting } from '../../store/reducers';
+import { AuthStateInterface } from '../../types/authState.interface';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   imports: [ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
   private fb = inject(FormBuilder);
-  private store = inject(Store);
+  private store = inject(Store<{ auth: AuthStateInterface }>);
+
+  isSubmitting = this.store.selectSignal(selectIsSubmitting);
 
   form = this.fb.nonNullable.group({
     username: ['', Validators.required],
---
+++ medium-clone-angular/src/app/auth/store/selectors.ts
@@ -0,0 +1,10 @@
+// кажуть що це зовсім не потрібно і сам ngrx це може робити без нас
+
+// import { AuthStateInterface } from '../types/authState.interface';
+// import { createSelector } from '@ngrx/store';
+//
+// export const selectFeature = (state: { auth: AuthStateInterface }) => state.auth;
+// export const selectIsSubmitting = createSelector(
+//   selectFeature,
+//   (state) => state.isSubmitting,
+// );
